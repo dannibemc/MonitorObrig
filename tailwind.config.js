0e2f5f2c886d6a82594fab00aabf690e5de86ad1
Danni.BE <db.mercadodecapitais@gmail.com>
2025-06-23 12:10:18 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#343E83',
+        secondary: '#32BECB',
+        tertiary: '#5DACCD',
+        accent: '#9E36EF',
+      },
+    },
+  },
+  plugins: [],
+};
